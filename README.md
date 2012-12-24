04224bceacf7eeae4e0e9cb1d06d249f4be71d8f
David Illsley <david@illsley.org>
2012-12-24 10:44:27 +0000

    Add a couple of comments...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Experimentation with scala macros and mongo
+
+The Macros are in macros/ and the code which uses them in core/ as macros and their users have to be in separate compilation units
--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -7,10 +7,13 @@ object Test extends App {
   
   val q = Macros.createHelper[AA]
   
+  // Below are a bunch of valid queries.
   q("part2").eql(9)
   q("part3.part4").eql("")
   q("part3").eql(B("", None))
   q("part3").eql(B("", Some("")))
   q("part3.part5").eql(Some(""))
-
+  // and now some invalid ones which won't compile...
+  //q("part2").eql("9")
+  //q("part22").eql(9)
 }
